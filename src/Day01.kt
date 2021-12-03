8054b090a72959714bec2c502f1dca8c64520709
Arjix <53124886+ArjixWasTaken@users.noreply.github.com>
2021-12-03 19:39:27 +0200

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,10 +1,37 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increasements = 0
+        var lastDepth: Int? = null
+
+        for (line in getLines()) {
+            if (line.isNotEmpty()) {
+                if (lastDepth == null) {
+                    lastDepth = line.toInt()
+                    continue
+                }
+
+                if (line.toInt()!! > lastDepth!!) {
+                    ++increasements
+                }
+                lastDepth = line.toInt()
+            }
+        }
+        return increasements
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increasements = 0
+        val lines = getLines()
+
+        for (i in 2 until lines.size - 1) {
+            val groupA = lines[i - 2].toInt()!! + lines[i - 1].toInt()!! + lines[i].toInt()!!
+            val groupB = lines[i - 1].toInt()!! + lines[i].toInt()!! + lines[i + 1].toInt()!!
+
+            if (groupA < groupB) {
+                ++increasements
+            }
+        }
+        return increasements
     }
 
     // test if implementation meets criteria from the description, like:
